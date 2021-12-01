d40fb5d0b68f2fad8844e6608f10e401590c1218
ajoy267 <andrew.joy12@hotmail.com>
2021-11-30 16:49:57 -0800

    changed footer to bottom

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 import './App.css'
 
 import Header from './components/top/Header'
-import Footer from './components/Footer/Footer'
+import Footer from './components/bottom/Footer'
 import Main from './components/Main/Main'
 
 function App() {
